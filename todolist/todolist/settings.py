95f2d752598ef30d7df81841c6d0fda3807a0f38
alllvp <alllvp@gmail.com>
2022-12-07 15:39:28 +0300

    add static

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "static"
+STATIC_ROOT = BASE_DIR.joinpath('static')
 
 
 # Default primary key field type
